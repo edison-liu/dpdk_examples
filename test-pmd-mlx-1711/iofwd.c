21ffaf6e44bf8d930fdd26ea0374177aecafecfc
Edison Liu <7872232@qq.com>
2020-08-25 20:48:37 +0800

    add TCP ingress flow

--- test-pmd-mlx-1711/iofwd.c
+++ test-pmd-mlx-1711/iofwd.c
@@ -95,7 +95,7 @@ static void handle_burst(struct rte_mbuf **pkts_burst, uint16_t nb_rx)
 		tmp = in_ipv4_hdr->src_addr;
 		in_ipv4_hdr->src_addr = in_ipv4_hdr->dst_addr;
 		in_ipv4_hdr->dst_addr = tmp;
-		//printf("Inner IP %08x -> %08x\n", in_ipv4_hdr->src_addr, in_ipv4_hdr->dst_addr);
+		printf("Inner IP %08x -> %08x\n", in_ipv4_hdr->src_addr, in_ipv4_hdr->dst_addr);
 	}
 }
 
